9bca641ca20a57e32a052a91a3a670b2336f31e0
chgmxx <chenguiming@gmail.com>
2016-02-29 13:03:10 +0800

    增加权限判断测试代码，使用enforcePermission接口

--- app/src/main/java/com/scranol/ajeet/button/MainActivity.java
+++ app/src/main/java/com/scranol/ajeet/button/MainActivity.java
@@ -1,12 +1,16 @@
 package com.scranol.ajeet.button;
 
 import android.content.Intent;
+import android.os.SystemClock;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+import android.os.Process;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -17,8 +21,13 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
     }
     public void sendMessage(View view){
-       Intent intent = new Intent(this,DisplayMessageActivity.class);
-        EditText editText= (EditText) findViewById(R.id.edit_message);
+//       Intent intent = new Intent(this,DisplayMessageActivity.class);
+//        EditText editText= (EditText) findViewById(R.id.edit_message);
+        checkMyPermission("android.permission.ACCESS_GPS");
+        SystemClock.sleep(3000);
+        checkMyPermission("android.permission.ACCESS_COARSE_LOCATION");
+        SystemClock.sleep(3000);
+        checkMyPermission("android.permission.ACCESS_FINE_LOCATION");
 
     }
 
@@ -42,5 +51,20 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    private boolean checkMyPermission(String permisssion){
+        int pid =Process.myPid();
+        int uid = Process.myUid();
+        try{
+            enforcePermission(permisssion,pid,uid,"Touch not allow");
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            Toast.makeText(this, permisssion + "not XXXXXX allow", Toast.LENGTH_LONG).show();
+            Log.e("cgm", permisssion + " XXXXXX not allow");
+            return false;
+        }
+        Log.e("cgm",permisssion + " oooooo allow");
+        return true;
     }
 }
